39f9706a76832d1603357ff1a2d86a25e9c35651
juna desktop <wnsghk6670@naver.com>
2021-10-10 20:54:10 +0900

    SpringBoot 공부용 프로젝트 생성 eclipse maven, java8

---
+++ src/main/java/com/sb/study/BootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.sb.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/study/BootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
