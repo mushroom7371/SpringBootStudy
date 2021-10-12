eaefe6abb39ffc8aedfe2be51374a4bf238c5062
juna desktop <wnsghk6670@naver.com>
2021-10-12 20:48:50 +0900

    SpringBoot - oracle, mybatis 연동을 위한 디펜던시 추가 및 간단 주석

--- src/main/java/com/sb/study/BootStudyApplication.java
+++ src/main/java/com/sb/study/BootStudyApplication.java
@@ -3,11 +3,14 @@ package com.sb.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성이 모두 가능하다.
+//해당 어노테이션이 있는 부분부터 설정을 읽어가기 때문에, 해당 어노테이션이 있는 클래스는 항상 프로젝트의 최상단에 위치해야한다.
 @SpringBootApplication
 public class BootStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootStudyApplication.class, args);
+		//run()메서드로 인하여 SpringBoot의 내장 WAS를 실행한다. (따로 톰캣설치할 필요가 없음)
 	}
 
 }
--- src/main/java/com/sb/study/SampleController.java
+++ src/main/java/com/sb/study/SampleController.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@EnableAutoConfiguration
+@Controller					//MVC 패턴중 C, 사용자 요청을 처리한 후 지정된 뷰에 모델을 넘겨주는 역할을 함. 어노테이션을 통하여 해당클래스를 controller로 사용한다고 명시
+@EnableAutoConfiguration	// @EnableAutoConfiguration 어노테이션을 통해서 스프링부트(SpringBoot)의 설정을 자동으로 잡아준다.
 public class SampleController {
 	
-	@RequestMapping(value="/")
-	@ResponseBody
+	@RequestMapping(value="/")	//들어온 요청을 특정 메서드와 매핑하기 위해서 사용
+	@ResponseBody				//java 객체를 HTTP 요청의 body 내용으로 매핑하는 역할. VO 객체를 JSON으로 바꿔서 HTTP body에 담는 스프링 어노테이션
     public String sampleHome() {
 		
-        return "Hello Spring Boot!";
+        return "Hello Spring Boot!";	//메서드 선언시 String 타입을 반환하도록 명시하였기에 return 또한 문자열이 된다.
         
     }
 }
