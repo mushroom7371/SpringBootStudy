2b9caa1c890174adb3ffe48a89eca53b236760e4
juna desktop <wnsghk6670@naver.com>
2021-10-14 22:01:06 +0900

    SpringBoot - index page 제작중, controller에서 jsp 파일을 불러오지 못함...

--- src/main/java/com/sb/study/controller/IndexController.java
+++ src/main/java/com/sb/study/controller/IndexController.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.sb.study.vo.StudyVo;
 import com.sb.study.service.StudyService;
@@ -17,12 +20,20 @@ import com.sb.study.service.StudyService;
 //@Controller					//MVC 패턴중 C, 사용자 요청을 처리한 후 지정된 뷰에 모델을 넘겨주는 역할을 함. 어노테이션을 통하여 해당클래스를 controller로 사용한다고 명시
 //@EnableAutoConfiguration	// @EnableAutoConfiguration 어노테이션을 통해서 스프링부트(SpringBoot)의 설정을 자동으로 잡아준다.
 @RestController
-public class SampleController {
+public class IndexController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(){
+		System.out.println("controller start");
+		
+		return "index";
+	}
+
 	
 	@Autowired
 	StudyService StudyService; 
 	
-	@RequestMapping(value="/")	//들어온 요청을 특정 메서드와 매핑하기 위해서 사용
+//	@RequestMapping(value="/")	//들어온 요청을 특정 메서드와 매핑하기 위해서 사용
  	@ResponseBody //java 객체를 HTTP 요청의 body 내용으로 매핑하는 역할. VO 객체를 JSON으로 바꿔서 HTTP body에 담는 스프링 어노테이션
     public List<StudyVo> sampleHome(@ModelAttribute("searchVO") StudyVo Vo, ModelMap model) throws Exception {
 		
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
+<!DOCTYPE html>
+<html lang='ko'>
+<head>
+<meta charset="UTF-8">
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>index</title>
+</head>
+<body>
+	<h2>인덱스</h2>
+	
+	<input type='button' id='btnSearch' value='DB조회 테스트 버튼'/>
+
+</body>
+</html>
