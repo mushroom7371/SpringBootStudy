da3f654c40ef79dfdbba57b6fb4a843cdb9d8a09
juna desktop <wnsghk6670@naver.com>
2021-10-17 18:33:51 +0900

    SpringBoot - index페이지 구현 성공, @RestController는 json으로 반환하기 때문에 jsp 화면이 호출되지 않음

--- src/main/java/com/sb/study/controller/IndexController.java
+++ src/main/java/com/sb/study/controller/IndexController.java
@@ -19,26 +19,29 @@ import com.sb.study.service.StudyService;
 
 //@Controller					//MVC 패턴중 C, 사용자 요청을 처리한 후 지정된 뷰에 모델을 넘겨주는 역할을 함. 어노테이션을 통하여 해당클래스를 controller로 사용한다고 명시
 //@EnableAutoConfiguration	// @EnableAutoConfiguration 어노테이션을 통해서 스프링부트(SpringBoot)의 설정을 자동으로 잡아준다.
-@RestController
+//@RestController를 사용하면 json으로 반환하기 때문에 jsp 화면이 호출되지 않음, Controller를 사용해야됨
+@Controller
 public class IndexController {
 	
+	//매핑을 통하여 url이 /로 들어오면 실행되는 메서드
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(){
-		System.out.println("controller start");
+		System.out.println("controller start");	//컨트롤러로 잘 들어오는지 확인용 출력
 		
-		return "index";
+		return "index";	//localhost로 들어오면 바로 jsp를 띄우도록 하였음. properties의 pre,suf fix로 경로를 지정한 index파일을 띄울수 있도록 함
+		//jsp 호출은 Contorller임...
 	}
 
 	
-	@Autowired
-	StudyService StudyService; 
-	
+//	@Autowired
+//	StudyService StudyService; 
+//	
 //	@RequestMapping(value="/")	//들어온 요청을 특정 메서드와 매핑하기 위해서 사용
- 	@ResponseBody //java 객체를 HTTP 요청의 body 내용으로 매핑하는 역할. VO 객체를 JSON으로 바꿔서 HTTP body에 담는 스프링 어노테이션
-    public List<StudyVo> sampleHome(@ModelAttribute("searchVO") StudyVo Vo, ModelMap model) throws Exception {
-		
-		List<StudyVo> eList = StudyService.selectStudyMember(Vo);
-
-		return eList;    
-    }
+// 	@ResponseBody //java 객체를 HTTP 요청의 body 내용으로 매핑하는 역할. VO 객체를 JSON으로 바꿔서 HTTP body에 담는 스프링 어노테이션
+//    public List<StudyVo> sampleHome(@ModelAttribute("searchVO") StudyVo Vo, ModelMap model) throws Exception {
+//		
+//		List<StudyVo> eList = StudyService.selectStudyMember(Vo);
+//
+//		return eList;    
+//    }
 }
