67d5ba015fb66a37cb96ba35afbb104934bfade1
juna desktop <wnsghk6670@naver.com>
2021-10-27 20:25:24 +0900

    SpringBoot - 페이징 처리 사전작업, PagingVo 추가

---
+++ src/main/java/com/sb/study/vo/PagingVo.java
@@ -0,0 +1,58 @@
+package com.sb.study.vo;
+
+public class PagingVo {
+	
+	//현재,	,시작,		끝,	,게시글 총갯수,페이지당 글 갯수,마지막,		쿼리에서 쓸 start,end
+	private int nowPage, startPage, endPage, total, cntPerPage, lastPage, start, end;
+	private int cntPage = 5;
+		
+	public int getNowPage() {
+		return nowPage;
+	}
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+	}
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getCntPerPage() {
+		return cntPerPage;
+	}
+	public void setCntPerPage(int cntPerPage) {
+		this.cntPerPage = cntPerPage;
+	}
+	public int getLastPage() {
+		return lastPage;
+	}
+	public void setLastPage(int lastPage) {
+		this.lastPage = lastPage;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+}
