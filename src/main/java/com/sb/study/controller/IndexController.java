144c116ae1e3dc1f536a1f0aaa1fbb827c93f2ab
juna desktop <wnsghk6670@naver.com>
2021-11-09 23:19:31 +0900

    SpringBoot - 파일 업로드 사전작업2

--- src/main/java/com/sb/study/controller/IndexController.java
+++ src/main/java/com/sb/study/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.sb.study.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -19,6 +24,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.sb.study.vo.PagingVo;
+import com.sb.study.vo.StudyAttVo;
 import com.sb.study.vo.StudyVo;
 import com.sb.study.dao.StudyDao;
 import com.sb.study.service.StudyService;
@@ -28,6 +34,8 @@ import com.sb.study.service.StudyService;
 //@RestController를 사용하면 json으로 반환하기 때문에 jsp 화면이 호출되지 않음, Controller를 사용해야됨
 @Controller
 public class IndexController {
+	
+	final static String saveDir = "C:\\eclipse\\workspace\\SpringBootStudy\\BootStudy\\src\\main\\webapp\\upload\\";
 	
 	@Autowired
 	StudyDao dao;
@@ -124,6 +132,48 @@ public class IndexController {
 		mav.setViewName("search");
 		
 		return mav;
+	}
+	
+public List<StudyAttVo> upload(HttpServletRequest req) {
+		
+		List<StudyAttVo> attList = null;
+		
+		try {
+			req.setCharacterEncoding("utf-8");
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmssSS");
+			
+			String contentType = req.getContentType();
+	
+			if(contentType.toLowerCase().startsWith("multipart/")) {
+				
+				attList = new ArrayList<StudyAttVo>();
+			
+				Collection<Part> parts = req.getParts();
+				for(Part p : parts) {
+					if( p.getHeader("Content-Disposition").contains("filename=")) { // file tag
+						String fileName = p.getSubmittedFileName();
+						StudyAttVo attVo = new StudyAttVo();
+						
+						String date = sdf.format(new Date());
+						
+						System.out.println("p에 담기는가?" + p.getSize());
+						
+						if(p.getSize()>0) {
+							p.write(saveDir + date + "-" + fileName);
+							p.delete();
+	
+							attVo.setSysAtt(date + "-" + fileName);
+							attVo.setOriAtt(fileName);
+							attList.add(attVo);
+						}
+					}
+				}
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return attList;
 	}
 
 	
